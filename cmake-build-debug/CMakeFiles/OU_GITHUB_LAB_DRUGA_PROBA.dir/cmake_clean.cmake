1766bab0eb51f049847334f7a28379ee7d3776f9
macszl <235989@edu.p.lodz.pl>
2021-02-01 17:02:51 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_GITHUB_LAB_DRUGA_PROBA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_GITHUB_LAB_DRUGA_PROBA.dir/main.cpp.obj"
+  "OU_GITHUB_LAB_DRUGA_PROBA.exe"
+  "OU_GITHUB_LAB_DRUGA_PROBA.exe.manifest"
+  "OU_GITHUB_LAB_DRUGA_PROBA.pdb"
+  "libOU_GITHUB_LAB_DRUGA_PROBA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_GITHUB_LAB_DRUGA_PROBA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OU_GITHUB_LAB_DRUGA_PROBA.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OU_GITHUB_LAB_DRUGA_PROBA.
+# This may be replaced when dependencies are built.
